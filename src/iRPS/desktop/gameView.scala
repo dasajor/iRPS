6e6ff4415cfe063ea395a7e9b4d024100b019572
Daniel Sajor <dasajor@buffalo.edu>
2019-05-06 16:22:11 -0400

    +desktop

--- src/iRPS/desktop/gameView.scala
+++ src/iRPS/desktop/gameView.scala
@@ -1,3 +1,5 @@
+package iRPS
+
 package iRPS.desktop
 
 import javafx.scene.input.{KeyCode, KeyEvent}
@@ -11,32 +13,42 @@ import io.socket.emitter.Emitter
 import play.api.libs.json.{JsValue, Json}
 import scalafx.scene.shape.{Circle, Rectangle}
 import scalafx.scene.paint.Paint
-import iRPS.model._
+import scalafx.scene.Group
+
 
 object gameView extends JFXApp {
-
-  var socket: Socket = IO.socket("http://localhost:8080/")
-  socket.on("gamestate", new HandleMessagesFromPython)
-  socket.connect()
 
 
   stage = new application.JFXApp.PrimaryStage {
     title.value = "Test"
     scene = new Scene(1280, 720) {
+      var socket: Socket = IO.socket("http://localhost:8080/")
+      socket.on("gameState", new HandleMessagesFromPython)
+      socket.connect()
+      val socketid = socket.id
+      var gigaGameState: JsValue = Json.obj()
+
+
+      class HandleMessagesFromPython() extends Emitter.Listener {
+        override def call(objects: Object*): Unit = {
+          val message = objects.apply(0).toString
+          val gameState: JsValue = Json.parse(message)
+          parseGameState(message)
+        }
+      }
+
+
+
+
+
+
+
 
 
       fill = White
 
+      var sceneGraphics: Group = new Group{}
       val tileSize = 30
-
-      def placeCircle(x: Double, y: Double, color: Paint, radius: Double): Unit = {
-        val circle0 = new Circle
-        circle0.radius = 6
-        circle0.centerX = x
-        circle0.centerY = y
-        circle0.fill = color
-      }
-
       def placeSquare(x: Double, y: Double, color: Paint): Unit = {
         val square0 = new Rectangle
         square0.width = tileSize
@@ -44,41 +56,105 @@ object gameView extends JFXApp {
         square0.x = x
         square0.y = y
         square0.fill = color
+        sceneGraphics.children.add(square0)
+        content = List(sceneGraphics)
       }
+      def placeCircle(x: Double, y: Double, color: String, radius: Double): Unit = {
+        val circle0 = new Circle
+        circle0.radius = radius
+        circle0.centerX = x
+        circle0.centerY = y
+        if (color == socket.id) {
+          circle0.fill = Blue
+        } else {
+          circle0.fill = Orange
+        }
+        sceneGraphics.children.add(circle0)
+        content = List(sceneGraphics)
+      }
+      def drawGameBoard(x: Double, y: Double): Unit = {
+        placeSquare(x, y, White)
+      }
+      def parseGameState(event: String): Unit = {
+        val parsed: JsValue = Json.parse(event)
+        val gridSize: Map[String, JsValue] = (parsed \ "gridSize").as[Map[String, JsValue]]
+        val player: List[JsValue] = (parsed \ "players").as[List[JsValue]]
 
 
+        //for (j <- gridSize) { Faulty code apparently
+        //  val gx = gridSize("x").as[Double]
+        //  val gy = gridSize("y").as[Double]
+        //  drawGameBoard(gx, gy)
+        //}
 
+        //for (i <- player) {
+        //  val px = (i \ "x").as[Double]
+        //  val py = (i \ "y").as[Double]
+        //  val playerid: Map[String, JsValue] = (parsed \ "id").as[Map[String, JsValue]]
+        //
+        //  for (d <- playerid) {
+        //    val id = d._2.as[String]
+
+        //    placeCircle(px, py, id, 2.0)
+        //  }
+
+
+        //}
+      } // gameparse ends
+
+
+      var keyStates: JsValue = Json.obj(
+        "w" -> false,
+        "a" -> false,
+        "s" -> false,
+        "d" -> false,
+      )
 
       def keyPressed(keyCode: KeyCode): Unit = {
         keyCode.getName match {
-          case "W" => println("Yup")
-          case "A" => println("Yup")
-          case "S" => println("Yup")
-          case "D" => println("Yup")
-          case _ => println(keyCode.getName + " pressed with no action")
+          case "W" => //checkState(keyCode, true)
+          case "A" => //checkState(keyCode, true)
+          case "S" => //checkState(keyCode, true)
+          case "D" => //checkState(keyCode, true)
+          case "Up" => //checkState(keyCode, true)
+          case "Left" => //checkState(keyCode, true)
+          case "Down" => //checkState(keyCode, true)
+          case "Right" => //checkState(keyCode, true)
+          case _ => print()
         }
       }
 
-      content = List()
+      //def checkState(keyCode: KeyCode, Boolean: Boolean): Unit = {
+      //  if (keyStates(keyCode.getName) != Boolean) {
+      //  keyStates(keyCode.getName) = Boolean
+      //    socket.emit("keyStates", Json.stringify(keyStates))
+      //  }
+      //}
+
+      def keyReleased(keyCode: KeyCode): Unit = {
+        keyCode.getName match {
+          case "W" => //checkState(keyCode, false)
+          case "A" => //checkState(keyCode, false)
+          case "S" => //checkState(keyCode, false)
+          case "D" => //checkState(keyCode, false)
+          case "Up" => //checkState(keyCode, false)
+          case "Left" => //checkState(keyCode, false)
+          case "Down" => //checkState(keyCode, false)
+          case "Right" => print("Button up")//checkState(keyCode, false)
+          case _ => print()
+        }
+      }
 
       addEventHandler(KeyEvent.KEY_PRESSED, (event: KeyEvent) => keyPressed(event.getCode))
+
+      addEventHandler(KeyEvent.KEY_RELEASED, (event: KeyEvent) => keyReleased(event.getCode))
+
     }
   }
+
+
+
+
+
 }
 
-class HandleMessagesFromPython() extends Emitter.Listener {
-  override def call(objects: Object*): Unit = {
-    val message = objects.apply(0).toString
-    // do something with message
-    Platform.runLater(() => {
-      val jsonGameState = objects.apply(0).toString
-      println(jsonGameState)
-      val gameState: JsValue = Json.parse(jsonGameState)
-      val player = (gameState \ "players").as[Map[String, JsValue]]
-
-      for ((k, v) <- player) {
-
-      }
-    })
-  }
-}
